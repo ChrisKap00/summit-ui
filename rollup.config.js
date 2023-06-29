a390207e5d39f3d1f0c2416e751f38785214aeff
Christos Kapotos <chrkap7@gmail.com>
2023-06-30 02:14:45 +0300

    added dts file

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import { babel } from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
-import dts from "rollup-plugin-dts";
-// import commonjs from "@rollup/plugin-commonjs";
 
 export default [
   {
@@ -11,11 +9,9 @@ export default [
     output: [
       { file: "dist/index.js", format: "cjs" },
       { file: "dist/index.es.js", format: "es", exports: "named" },
-      // { file: "dist/index.d.ts", format: "es" },
     ],
     plugins: [
       resolve(),
-      // commonjs(),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
@@ -23,7 +19,6 @@ export default [
       }),
       external(),
       terser(),
-      // dts(),
     ],
   },
 ];
