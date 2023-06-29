8714bfc5ab19ab6c78b49a8da0dc066c0d59345f
Christos Kapotos <chrkap7@gmail.com>
2023-06-30 01:06:33 +0300

    troubleshooting types

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import { babel } from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+// import commonjs from "@rollup/plugin-commonjs";
 
 export default [
   {
@@ -9,9 +11,11 @@ export default [
     output: [
       { file: "dist/index.js", format: "cjs" },
       { file: "dist/index.es.js", format: "es", exports: "named" },
+      // { file: "dist/index.d.ts", format: "es" },
     ],
     plugins: [
       resolve(),
+      // commonjs(),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
@@ -19,6 +23,7 @@ export default [
       }),
       external(),
       terser(),
+      // dts(),
     ],
   },
 ];
